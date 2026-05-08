459af656a9349b98fc7eeff1829d08742ed0dc15
matthew kondratowicz <matthew.kond@gmail.com>
2026-05-07 22:34:23 -0400

    added balancing to model aswell as confusion matrix

--- main.py
+++ main.py
@@ -4,22 +4,34 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
+from imblearn.over_sampling import SMOTE
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
 
-df = pd.read_csv('C:\\Users\\matth\\Desktop\\creditcard.csv', encoding='latin-1')
+
+df = pd.read_csv('C:\\Users\\matth\\OneDrive\\Desktop\\creditcard.csv', encoding='latin-1')
 
 X = df.drop(columns=['Class'])
 y = df['Class']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+smote = SMOTE(random_state=42)
+X_train, y_train = smote.fit_resample(X_train, y_train)
 
 pipeline = Pipeline([
     ('scale', StandardScaler()),
-     ("model", LogisticRegression())
+     ("model", LogisticRegression(class_weight='balanced')),
 ])
 
-pipeline.fit(X_train, y_train)
+pipeline.fit(X_train, y_train) 
 
 predictions = pipeline.predict(X_test)
+
+cm = confusion_matrix(y_test, predictions)
+sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
+plt.title('Confusion Matrix')
+plt.show()
 
 print(classification_report(y_test,predictions))
